d6ed98d67c8f59397ce7a25e3b517a2fe2bf0b36
Yago Fogaça <yagofogaca20@gmail.com>
2025-03-05 10:58:11 -0300

    fix: correção dos estados do componente Btn

--- src/components-style/btn-primary/index.btn-primary.styled.js
+++ src/components-style/btn-primary/index.btn-primary.styled.js
@@ -5,8 +5,9 @@ export const BtnStyled = styled(Button)`
   background-color: #aa0cff;
   border-color: #aa0cff;
 
-  &:active {
-    border-color: #aa0cff;
-    background-color: #aa0cff;
+  &:active,
+  &:hover {
+    border-color: #aa0cff !important;
+    background-color: rgba(170, 12, 255, 0.83) !important;
   }
 `;
