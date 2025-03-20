6ad0741b5801d551290c4569e845c7c7fc1f0918
Yago Fogaça <yagofogaca20@gmail.com>
2025-03-20 14:07:05 -0300

    feat: criação do componente de deletar movimentação

---
+++ src/components/delete-movement/index.delete-movement.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { Api } from "../../api/index.api.user.js";
+import { HandlingMovementContext } from "../../utils/handling-movement-context/index.handling-movement-context.js";
+import { MovementContext } from "../../context/movement/index.context.movement.jsx";
+
+export const DeleteMovement = ({ movement_id, index }) => {
+  const { movements, setMovements, setMovementsAmount } =
+    useContext(MovementContext);
+
+  const deleteMovement = async () => {
+    try {
+      await Api.deleteMovement(movement_id);
+      setMovements(
+        HandlingMovementContext.removeMovementContext(movements, index)
+          .movements
+      );
+      setMovementsAmount(
+        HandlingMovementContext.removeMovementContext(movements, index).amount
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={deleteMovement}>
+        <i className="bi bi-trash3-fill"></i>
+      </button>
+    </>
+  );
+};
--- src/components/table-movements/index.table-movements.jsx
+++ src/components/table-movements/index.table-movements.jsx
@@ -1,28 +1,8 @@
 import * as C from "./index.table-movements.styled.js";
 import * as I from "../../components-style/div-icons/index.div-icons";
-import { HandlingMovementContext } from "../../utils/handling-movement-context/index.handling-movement-context.js";
-import { useContext } from "react";
-import { MovementContext } from "../../context/movement/index.context.movement.jsx";
-import { Api } from "../../api/index.api.user.js";
+import { DeleteMovement } from "../delete-movement/index.delete-movement.jsx";
 
 export const TableMovements = ({ movements }) => {
-  const { setMovements, setMovementsAmount } = useContext(MovementContext);
-
-  const deleteMovement = async (movement_id, index) => {
-    try {
-      await Api.deleteMovement(movement_id);
-      setMovements(
-        HandlingMovementContext.removeMovementContext(movements, index)
-          .movements
-      );
-      setMovementsAmount(
-        HandlingMovementContext.removeMovementContext(movements, index).amount
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
       <C.TableStyled striped responsive>
@@ -67,13 +47,7 @@ export const TableMovements = ({ movements }) => {
                   <button>
                     <i className="bi bi-pencil-fill"></i>
                   </button>
-                  <button
-                    onClick={() => {
-                      deleteMovement(movement.id, index);
-                    }}
-                  >
-                    <i className="bi bi-trash3-fill"></i>
-                  </button>
+                  <DeleteMovement index={index} movement_id={movement.id} />
                 </C.TdStyled>
               </tr>
             );
